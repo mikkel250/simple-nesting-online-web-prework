58d706a2b538cdc410f5073209fd61c5a21e7bc3
Mikkel Ridley <mikkel250@gmail.com>
2018-09-10 23:49:46 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,7 +17,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,9 +38,12 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -55,6 +59,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     	binding.pry
+programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
@@ -66,6 +72,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		   
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,7 +87,7 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
