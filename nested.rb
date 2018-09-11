6bfa4234839309201676bab1c911a6613b0f8e30
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 01:14:34 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -59,8 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     	binding.pry
-programmer_hash[:dennis_ritchie][:languages].to_s
+ #    	binding.pry
+programmer_hash[:dennis_ritchie][:languages].join
+
 end
 
 def adding_matz
@@ -88,6 +89,7 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
@@ -109,7 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -128,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
