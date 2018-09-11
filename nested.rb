1255ccd431865558c235e342b817551c67fb4dda
Mikkel Ridley <mikkel250@gmail.com>
2018-09-11 01:06:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         }
      }
  #    	binding.pry
-lang = programmer_hash[:dennis_ritchie][:languages]
+lang = programmer_hash[:dennis_ritchie][:languages].split
 end
 
 def adding_matz
